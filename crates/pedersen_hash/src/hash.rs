0e4323e8361962c3818c388fb98d1581eb343096
Krisztian Kovacs <krisztian@equilibrium.co>
2022-04-25 13:52:48 +0200

    tmp: generate curve consts build time

--- crates/pedersen_hash/src/hash.rs
+++ crates/pedersen_hash/src/hash.rs
@@ -3,13 +3,12 @@ use std::error::Error;
 use pedersen_curve::{
     AffinePoint, ProjectivePoint, PEDERSEN_P0, PEDERSEN_P1, PEDERSEN_P2, PEDERSEN_P3, PEDERSEN_P4,
 };
-use crate::curve_consts::{
-    CURVE_CONSTS_BITS, CURVE_CONSTS_P1, CURVE_CONSTS_P2, CURVE_CONSTS_P3, CURVE_CONSTS_P4,
-};
 use pedersen_curve::{FieldElement, FieldElementRepr};
 
 use bitvec::{field::BitField, order::Msb0, slice::BitSlice, view::BitView};
 use ff::PrimeField;
+
+include!(concat!(env!("OUT_DIR"), "/curve_consts.rs"));
 
 /// The Starknet elliptic curve Field Element.
 ///
